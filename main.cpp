a309928259057e69c7e3aa5e1a144507075b01d3
Nam-seonwoo <nsw88@naver.com>
2020-12-16 00:51:26 +0900

    add input

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
+    char name[20];
     cout<<"Hello world"<<endl;
+    cout<<"your name : ";
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
     return 0;
 }
